08cfb6fde440e695b40fe387c000cb1cc42e8dbb
tharcissie <tharcissieidufashe@gmail.com>
2020-11-26 17:41:21 +0200

    myapp

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MOVIE_API_KEY=fd22e40c8c6180ebf3799ca28920527b
+export SECRET_KEY=tharcissie
+export MAIL_USERNAME=tharcissieidufashe@gmai.com
+export MAIL_PASSWORD=ntakarakorwa123
+
+python3.6 manage.py server
